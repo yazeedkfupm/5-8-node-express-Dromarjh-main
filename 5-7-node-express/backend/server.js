82aea89f1e98ecce45c715529ce586044064f18f
Almutairi <s202164390@ccm.kfupm.edu.sa>
2025-11-05 09:30:18 +0300

    task 5 9:30am

--- 5-7-node-express/backend/server.js
+++ 5-7-node-express/backend/server.js
@@ -4,16 +4,20 @@ import { getRandomQuote } from "./quotes.js";
 
 const app = express();
 app.use(cors());
-const PORT = 3000;
 
-app.get("/quote", (req, res) => {
-  const quote = getRandomQuote();
-  res.json(quote);
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the Quote Generator API");
 });
 
 
+app.get("/api/quote", (req, res) => {
+  const quote = getRandomQuote();
+  res.json({ quote });
+});
+
+
+const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
-
-
