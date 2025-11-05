094df35b28d262868cc0de0fa05f9c6a91971c3e
Almutairi <s202164390@ccm.kfupm.edu.sa>
2025-11-05 09:12:20 +0300

    task 1 9:11am

--- 5-7-node-express/backend/server.js
+++ 5-7-node-express/backend/server.js
@@ -2,11 +2,17 @@ import express from "express";
 import cors from "cors";
 import { getRandomQuote } from "./quotes.js";
 
-{/*write code for cors*/}
+const app = express();
+app.use(cors());
 
+app.get("/quote", (req, res) => {
+  const quote = getRandomQuote();
+  res.json(quote);
+});
 
-{/*write code to define routes*/}
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
 
-
-{/*write code to create server*/}
 
